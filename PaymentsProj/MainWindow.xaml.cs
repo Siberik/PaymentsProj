a24b465737470a37fa59d0949810cd847d01e52c
user25 <user25@user25.ru>
2023-01-13 13:15:10 +0500

    Добавьте файлы проекта.

---
+++ PaymentsProj/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using PaymentsProj.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Excel = Microsoft.Office.Interop.Excel;
+namespace PaymentsProj
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Core db = new Core();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonClick(object sender, RoutedEventArgs e)
+        {
+            var allUsers = db.context.Users.ToList().OrderBy(p => p.last_name).ToList();
+
+            var application = new Excel.Application();
+            application.SheetsInNewWorkbook = allUsers.Count();
+
+            Excel.Workbook workbook = application.Workbooks.Add(Type.Missing);
+        }
+    }
+}
