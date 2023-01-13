ba795c69b03398fa457e9b93368a851cf64f2752
user25 <user25@user25.ru>
2023-01-13 13:54:44 +0500

    промежуточный этап

--- PaymentsProj/MainWindow.xaml.cs
+++ PaymentsProj/MainWindow.xaml.cs
@@ -32,9 +32,29 @@ namespace PaymentsProj
             var allUsers = db.context.Users.ToList().OrderBy(p => p.last_name).ToList();
 
             var application = new Excel.Application();
+            application.Visible = true;
+
             application.SheetsInNewWorkbook = allUsers.Count();
 
             Excel.Workbook workbook = application.Workbooks.Add(Type.Missing);
+            for(int i =0; i< allUsers.Count();i++)
+            {
+                int startRowIndex = 1;
+                Excel.Worksheet worksheet = application.Worksheets.Item[i + 1];
+                worksheet.Name = allUsers[i].last_name;
+
+                worksheet.Cells[1][startRowIndex] = "Дата платежа";
+                worksheet.Cells[2][startRowIndex] = "Название";
+                worksheet.Cells[3][startRowIndex] = "Стоимость";
+                worksheet.Cells[4][startRowIndex] = "Количество";
+                worksheet.Cells[5][startRowIndex] = "Сумма";
+
+                startRowIndex++;
+
+                var usersCategories = allUsers[i].Payment.OrderBy(p => p.date_payment).GroupBy(p => p.Category).OrderBy(p => p.Key.name_category);
+
+
+            }
         }
     }
 }
